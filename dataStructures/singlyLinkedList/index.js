c2650ab23d356e393062155fd81c219116fcaac0
KesavCodes <kesavakumarcode@outlook.com>
2025-08-04 22:27:56 +0530

    [SinglyLinkedList methods - 3]

--- dataStructures/singlyLinkedList/index.js
+++ dataStructures/singlyLinkedList/index.js
@@ -13,6 +13,7 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  
   push(val) {
     const node = new Node(val);
     if (this.head === null) {
@@ -25,6 +26,7 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
   isListEmpty() {
     if (this.length === 0) {
       console.log("List is Empty!");
@@ -32,6 +34,7 @@ class SinglyLinkedList {
     }
     return false;
   }
+
   isListHasOnlyOneNode() {
     if (this.length === 1) {
       const current = this.head;
@@ -42,6 +45,7 @@ class SinglyLinkedList {
     }
     return null;
   }
+
   pop() {
     if (this.isListEmpty()) return null;
     const handleSingleNode = this.isListHasOnlyOneNode();
@@ -57,6 +61,7 @@ class SinglyLinkedList {
     this.length--;
     return current;
   }
+
   unshift(val) {
     const node = new Node(val);
     if (this.head === null) {
@@ -69,6 +74,7 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
   shift() {
     if (this.isListEmpty()) return null;
     const handleSingleNode = this.isListHasOnlyOneNode();
@@ -81,7 +87,7 @@ class SinglyLinkedList {
   }
 
   get(index) {
-    if (index > this.length - 1 || index < 0) {
+    if (index >= this.length || index < 0) {
       console.log(
         `Invalid Index. Total list length is ${
           this.length
@@ -95,8 +101,30 @@ class SinglyLinkedList {
       current = current.next;
       currentIndex++;
     }
-    return current.val;
+    return current;
   }
+
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode) foundNode.val = val;
+    return foundNode;
+  }
+
+  insert(index, val) {
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+    const prevNode = this.get(index - 1);
+    if (prevNode) {
+      const temp = prevNode.next;
+      const newNode = new Node(val);
+      newNode.next = temp;
+      prevNode.next = newNode;
+      this.length++;
+      return true;
+    }
+    return false;
+  }
+
   traverse() {
     let current = this.head;
     while (current) {
@@ -111,26 +139,35 @@ const sLinkedList = new SinglyLinkedList();
 sLinkedList.push("Hi");
 sLinkedList.push("There");
 sLinkedList.push("How have you been?");
-sLinkedList.traverse();
+// sLinkedList.traverse();
 
 console.log(sLinkedList.get(0), "--get index 0");
 console.log(sLinkedList.get(2), "--get index 2");
 console.log(sLinkedList.get(3), "--get index 3");
 
-console.log(sLinkedList.pop());
-console.log(sLinkedList.pop());
-console.log(sLinkedList.pop());
-console.log(sLinkedList.pop());
+sLinkedList.insert(1, "Hello from one");
 
-sLinkedList.unshift("I am back");
-sLinkedList.unshift("Back Again");
-sLinkedList.unshift("So Back");
-sLinkedList.traverse();
+// sLinkedList.set(2, 'I am good');
+// sLinkedList.set(0, 'Hello');
+// sLinkedList.set(3, 'Test');
 
-console.log(sLinkedList.shift());
-console.log(sLinkedList.shift());
-console.log(sLinkedList.shift());
-console.log(sLinkedList.shift());
+console.log(sLinkedList.get(3), "--get index 3");
+// console.log(sLinkedList.get(2), "--get index 2");
+
+// console.log(sLinkedList.pop());
+// console.log(sLinkedList.pop());
+// console.log(sLinkedList.pop());
+// console.log(sLinkedList.pop());
+
+// sLinkedList.unshift("I am back");
+// sLinkedList.unshift("Back Again");
+// sLinkedList.unshift("So Back");
+// sLinkedList.traverse();
+
+// console.log(sLinkedList.shift());
+// console.log(sLinkedList.shift());
+// console.log(sLinkedList.shift());
+// console.log(sLinkedList.shift());
 
 sLinkedList.traverse();
 
